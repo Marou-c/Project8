e2670386ac9ee40b906515f1b5993281235cd875
Marou-c <95153631+Marou-c@users.noreply.github.com>
2021-12-12 18:32:19 -0500

    Source commit

---
+++ Source/MyProject8/MU_Class_BCI.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MU_Class_BCI.h"
+
+// Sets default values
+AMU_Class_BCI::AMU_Class_BCI()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AMU_Class_BCI::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMU_Class_BCI::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
